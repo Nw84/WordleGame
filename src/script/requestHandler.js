7c67b3d0c1141ea9e0b163ae35f92e85d2d07e78
Nordwall <tobiasnordwall@gmail.com>
2022-03-24 09:46:18 +0100

    I have solved the logic for removing words with repeatable letters

--- src/script/requestHandler.js
+++ src/script/requestHandler.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
-import { parse } from "path";
 
-export async function loadSecretWord(secretwordlength) {
+export async function loadSecretWord(secretwordlength, uniqueSecretWord) {
     const res = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json");
     const payload = await res.json();
     const data = Object.keys(payload);
@@ -9,7 +8,21 @@ export async function loadSecretWord(secretwordlength) {
         return word.length === 5;
     })
 
-    return filteredData;
+    const hasRepeatedCharacters = str => new Set(str).size < str.length;
+
+    let uniqueWords = [];
+
+    //if (uniqueSecretWord === 1) {
+    for (let i = 0; i < filteredData.length; i++) {
+        let check = hasRepeatedCharacters(filteredData[i]);
+        if (check === false) {
+            uniqueWords.push(filteredData[i])
+        }
+    }
+
+    let secretWord = filteredData[Math.floor(Math.random() * filteredData.length)];
+
+    return secretWord;
 }
 
 export default loadSecretWord; 
